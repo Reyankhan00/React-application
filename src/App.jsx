b47f715fdac9b94edec5d7906cc9b64b3f4e78c3
unknown <qaimkhanireyan38@gmail.com>
2026-04-29 20:23:08 +0500

    Fix case sensitivity in imports

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,35 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import React from 'react';
+
+// Components ko import karte waqt dekhein folder 'Components' hai ya 'components'
 import Navbar from './Components/Navbar.jsx';
-import './Components/navbar.css'
 import Hero from './Components/Hero.jsx'
-import './Components/hero.css'
 import About from './Components/About.jsx'
-import './Components/about.css'
 import Projects from './Components/Projects.jsx'
-import './Components/projects.css'
 import Services from './Components/Services.jsx'
-import './Components/services.css'
 import Contact from './Components/Contact.jsx'
+
+// CSS Imports - Inka path aur spelling check karein
+// Agar error aye to check karein 'navbar.css' hai ya 'Navbar.css'
+import './Components/navbar.css'
+import './Components/hero.css'
+import './Components/about.css'
+import './Components/projects.css'
+import './Components/services.css'
 import './Components/contact.css'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-       <Hero />   {}
-       <About />
-       <Projects />
-       <Services />
-       <Contact />
+      <Hero />
+      <About />
+      <Projects />
+      <Services />
+      <Contact />
       
-      {/* Har section ki ID wahi rakhen jo Navbar links mein di hai */}
+      {/* Sections */}
       <section id="home" style={{height: '100vh'}}> <h1>Home Section</h1> </section>
       <section id="about" style={{height: '100vh'}}> <h1>About Me</h1> </section>
       <section id="projects" style={{height: '100vh'}}> <h1>My Projects</h1> </section>
@@ -35,13 +40,9 @@ function App() {
       <section id="contact" style={{height: '100vh'}}> <h1>Contact Me</h1> </section>
     </div>
   );
-   
 }
 
-
-
 export default App;
-
 
 
 
